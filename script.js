b24611558c864ee30a26281c993655b8a2075a19
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-11-04 19:27:18 +0900

    Change info method on Book object

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+        return `Title: ${title}, Author: ${author}, Pages: ${pages}, Read: ${read}`
     }
 }
 
@@ -14,10 +14,7 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const lotr = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
-
-addBookToLibrary(lotr);
+addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
 
 console.log(myLibrary);
-console.log('test');
 
