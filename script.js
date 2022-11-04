5091e35df1c6313791ecc9bbff9cede0b441a653
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-11-04 19:33:24 +0900

    Add starter books

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
+addBookToLibrary(new Book('Crime and Punishment', 'Fyodor Dostoevsky', 671, false));
+addBookToLibrary(new Book('The Plague', 'Albert Camus', 308, false));
+addBookToLibrary(new Book('The Divine Comedy', 'Dante Alighieri', 798, false));
+addBookToLibrary(new Book('Kafka on the Shore', 'Haruki Murakami', 467, true));
+addBookToLibrary(new Book('Hyperion', 'Dan Simmons', 500, true));
 
 console.log(myLibrary);
 
