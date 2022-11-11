1e6db7d04ad26ac1cbd2bc0d9c8c5ca6f607565c
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-11-11 20:09:31 +0900

    Add books in myLibrary into the DOM

--- script.js
+++ script.js
@@ -9,28 +9,44 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
+// Create method on Book object prototype
 Book.prototype.info = function() {
     return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.read}`
 }
 
 // Add book to library array
-function addBookToLibrary(title, author, pages, readStatus) {
+function addBook(title, author, pages, readStatus) {
   const book = new Book(title, author, pages, readStatus)
   myLibrary.push(book);
 }
 
 // Display each book
 function displayBooks() {
+  const cardWrapper = document.getElementById("card-wrapper")
   myLibrary.forEach(element => {
-    console.log(element);
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    // loop through keys in each Book object
+    for(const key in element) {
+      // ignore info() method
+      if(element.hasOwnProperty(key)) {
+        const div = document.createElement("div");
+        div.textContent = element[key];
+        console.log(element[key]);
+        console.log('test');
+        card.appendChild(div);
+      }
+    }
+    cardWrapper.append(card);
   });
 }
 
 // Add example books
-addBookToLibrary('Crime and Punishment', 'Fyodor Dostoevsky', 671, false);
-addBookToLibrary('The Plague', 'Albert Camus', 308, false);
-addBookToLibrary('The Divine Comedy', 'Dante Alighieri', 798, false);
-addBookToLibrary('Kafka on the Shore', 'Haruki Murakami', 467, true);
-addBookToLibrary('Hyperion', 'Dan Simmons', 500, true);
+addBook("Crime and Punishment", "Fyodor Dostoevsky", 671, false);
+addBook("The Plague", "Albert Camus", 308, false);
+addBook("The Divine Comedy", "Dante Alighieri", 798, false);
+addBook("Kafka on the Shore", "Haruki Murakami", 467, true);
+addBook("Hyperion", "Dan Simmons", 500, true);
 
 displayBooks();
